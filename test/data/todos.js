648b81ef4e689090f42b21b1830f7e87108c7534
Vasily93 <markovavasily@gmail.com>
2019-05-20 17:15:08 -0700

    initial set up

---
+++ test/data/todos.js
@@ -0,0 +1,24 @@
+cosnt todos = [
+    {
+        task: 'Learn JavaScript',
+        completed:true
+    },
+    {
+        task: 'Feed my Cats',
+        completed:true
+    },
+    {
+        task: 'Wash dishes',
+        completed:true
+    },
+    {
+        task: 'Takeout Trash',
+        completed:true
+    },
+    {
+        task: 'Learn Spanish',
+        completed:true
+    }
+];
+
+export default todos;
